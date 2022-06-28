6f237074c266f2fcb71ab62040801c7a40a28bcb
mani <100167532+Mani8217@users.noreply.github.com>
2022-06-28 12:02:47 +0100

    build calculator

---
+++ mani-project9/drumkit.js
@@ -0,0 +1,99 @@
+ <header>
+          <div class="logo">
+
+
+          <h1>Green Peace</h1>
+          <img src="./images/llog.jfif" width="100px" height="100px" alt="Greenpeace logo" />
+         </div>
+  
+       <nav id="headnav">
+          <ul >
+            <li class="tooltip" >
+              <a href="#" class="first2">Profile |</a>
+              <span class="tooltiptext">information about our group</span>
+            </li>
+            <li class="tooltip">
+              <a href="history.html" class="first2">History |</a>
+              <span class="tooltiptext">hitory of our works</span>
+            </li>
+            <li class="tooltip">
+              <a href="#">activities |</a
+              ><span class="tooltiptext"
+                >activities we have done around the world</span
+              >
+            </li>
+            <li class="tooltip">
+              <a href="#">Contact Us |</a
+              ><span class="tooltiptext"
+                >contact us by phone or send us an email</span
+              >
+            </li>
+            <li class="tooltip">
+              <a href="form.html">Sign Up</a>
+              <span class="tooltiptext">
+                join us in taking care of the world</span
+              >
+            </li>
+            
+            </ul>
+          </nav>
+          
+            <button type="button">
+          <i class="fa fa-bars"></i>
+            </button>
+  
+         
+         
+       
+      </div>
+
+         
+
+         <div class="ad">
+        <div class="col1 col">
+          
+          <a href="https://www.peta.org/international/"
+            >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Peta_logo.svg/368px-Peta_logo.svg.png"
+              alt="logo"
+              width="60px"
+              height="60px"
+          /></a>
+          
+        </div>
+        <div class="col2 col">
+         
+          <a href="https://www.iaws-web.org/"
+            >
+            
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6cT5So6pre4NXKRuI2MK5Vs8CTzDh7Wghow&usqp=CAU"
+              alt="logo"
+              width="60px"
+              height="60px"
+          /></a>
+          
+        </div>
+        <div class="col3 col">
+          <a href="https://www.worldanimalprotection.org/"
+            ><img
+              src="https://gumlet.assettype.com/theceo%2Fimport%2F2019%2F09%2FWorld-Animal-Welfare-Day.jpg?w=1200&auto=format%2Ccompress&ogImage=true"
+              alt="logo"
+              width="70px"
+              height="70px"
+          /></a>
+          
+        </div>
+        <div class="col4 col">
+          <a href="http://bearproject.org/site/show/id/15"
+            ><img class="clear"
+              src="https://media.4-paws.org/e/5/1/2/e512500484065774e5485bfb46f32d1f7bb04f4c/four_paws_4c.png"
+              alt="logo"
+              width="70px"
+              height="70px"
+          /></a>
+         
+        </div>
+      </div>
+   </header>
